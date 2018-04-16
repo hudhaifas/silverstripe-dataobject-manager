cc913b07b7600cbe5050b262c33bd0a5c009ab14
Hudhaifa Shatnawi <hudhaifa.shatnawi@gmail.com>
2018-04-16 16:23:25 -0500

    - Fixed some bugs

--- _config.php
+++ _config.php
@@ -1,12 +1,2 @@
 <?php
 
-use SilverStripe\Control\Director;
-
-/**
- * Fetches the name of the current module folder name.
- *
- * @return string
- */
-if (!defined('DATAOBJECT_MANAGER_DIR')) {
-    define('DATAOBJECT_MANAGER_DIR', ltrim(Director::makeRelative(realpath(__DIR__)), DIRECTORY_SEPARATOR));
-}

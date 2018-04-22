9c8ab21fd92d22dd744771cd703fc7c72c4d01bb
Hudhaifa Shatnawi <hudhaifa.shatnawi@gmail.com>
2018-04-22 14:53:03 -0500

    - Upgrading to SS4 & BS4  * Fixed namespace

--- src/pages/DataObjectPage.php
+++ src/pages/DataObjectPage.php
@@ -1,29 +1,8 @@
 <?php
 
-/*
- * MIT License
- *  
- * Copyright (c) 2017 Hudhaifa Shatnawi
- * 
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- * 
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
+namespace HudhaifaS\DOM;
 
+use Page;
 use SilverStripe\AssetAdmin\Forms\UploadField;
 use SilverStripe\Assets\Image;
 use SilverStripe\Forms\NumericField;
@@ -38,6 +17,7 @@ use SilverStripe\Forms\TextField;
 class DataObjectPage
         extends Page {
 
+    private static $table_name = 'DataObjectPage';
     private static $db = [
         'PageLength' => 'Int',
         'FbAppId' => 'Varchar(100)',
